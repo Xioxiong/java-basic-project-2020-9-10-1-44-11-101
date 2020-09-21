6b7a81aa09a55e81ba7684de00452fbb0dfe196d
xiongwei <2894001817@qq.com>
2020-09-21 18:19:45 +0800

    规则校验

--- src/main/java/com/thoughtworks/basic/Args.java
+++ src/main/java/com/thoughtworks/basic/Args.java
@@ -1,4 +1,25 @@
 package com.thoughtworks.basic;
 
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Args {
+    public String check(List<Schema> schemas){
+        Arg arg = new Arg();
+        arg.flagAndValueCheck(schemas);
+        for (Schema schema: schemas) {
+            ruleVerify(schema);
+        }
+        return "您输入的命令正确无误！";
+    }
+
+    private void ruleVerify(Schema schema) {
+        String flag = schema.getFlag();
+        Pattern p = Pattern.compile("-\\s+|-\\S\\S+|-\\S-");
+        Matcher m = p.matcher(flag);
+        if(m.find()){
+            throw new ArgException("-与flag之间有空格或参数之间没有空格");
+        }
+    }
 }
--- src/main/java/com/thoughtworks/basic/Schema.java
+++ src/main/java/com/thoughtworks/basic/Schema.java
@@ -2,5 +2,6 @@ package com.thoughtworks.basic;
 
 public interface Schema {
     public boolean judgeValueType(String value);
+    public String getFlag();
     public String getDefaultValue();
 }
